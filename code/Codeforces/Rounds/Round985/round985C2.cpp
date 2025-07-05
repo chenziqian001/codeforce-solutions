9684d98331d0791a93e84341decadf7f5c988c11
陈子谦 <626856811@qq.com>
2025-07-05 16:41:43 +0800

    完成分类

---
+++ code/Codeforces/Rounds/Round985/round985C2.cpp
@@ -0,0 +1,17 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+void solve(){
+
+
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--) solve();
+    system("pause");
+    return 0;
+}
+git add .
+git commit -m "完成分类"
+git push
