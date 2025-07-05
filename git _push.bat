2ff3f3e5d28b37568b2d834ff0ab80c881cd84db
陈子谦 <626856811@qq.com>
2025-07-05 16:55:38 +0800

    update at 05-2025-07_16-55

--- git _push.bat
+++ git _push.bat
@@ -1,5 +1,17 @@
 @echo off
+cd /d C:\Users\62685\Desktop\vc
+for /f "tokens=1-4 delims=/ " %%a in ("%DATE%") do (
+    set y=%%c
+    set m=%%a
+    set d=%%b
+)
+for /f "tokens=1-2 delims=:." %%a in ("%TIME%") do (
+    set h=%%a
+    set min=%%b
+)
+set CURTIME=%y%-%m%-%d%_%h%-%min%
+
 git add .
-git commit -m "update"
+git commit -m "update at %CURTIME%"
 git push
 pause
